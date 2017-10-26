96f00c8d743c34e3d38ecd7cabebadeb9f899399
Rufus <rufusasterisk@gmail.com>
2017-10-26 11:06:50 -0600

    Add header h1 and basic styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EndpointSearch from '../EndpointSearch/EndpointSearch';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <EndpointSearch
           className={'your-location'}
           searchTitle={'Your Location'} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+const Header = () => {
+
+  return (
+    <header>
+      <h1>Between<span>Friends</span></h1>
+    </header>
+  )
+}
+
+export default Header
