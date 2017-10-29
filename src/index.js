daf35172e383a4fd2a95b7a45b17178ab5840630
Rufus <rufusasterisk@gmail.com>
2017-10-29 17:03:32 -0600

    refactor for redux dispatch

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION___ &&
-  window.__REDUX_DEVTOOLS_EXTENSION___();
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(rootReducer, devTools, applyMiddleware(thunk));
 
 ReactDOM.render(
