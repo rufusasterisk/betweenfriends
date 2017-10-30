bc3522de3be99f48bd68a1ba2966708260483466
Rufus <rufusasterisk@gmail.com>
2017-10-30 17:47:59 -0600

    Add endpoint actions and reducers

--- src/actions/EndpointSearchActions.js
+++ src/actions/EndpointSearchActions.js
@@ -1,11 +1,29 @@
-export const updateIndividualLocation = (location, locationType) => ({
+export const updateIndividualLocation = (locationString, locationType) => ({
   type: 'UPDATE_LOCATION',
-  location,
+  locationString,
   locationType
 });
 
-export const shouldLoadMap = (option, mapType) => ({
+export const setGPSLocation = (latlngObject, locationType) => ({
+  type: 'SET_GPS_LOCATION',
+  GPS: latlngObject,
+  locationType
+});
+
+export const shouldLoadMap = (option, locationType) => ({
   type: 'LOAD_INDIVIDUAL_MAP',
   option,
-  mapType
+  locationType
+});
+
+export const displayMap = (option, locationType) => ({
+  type: 'DISPLAY_INDIVIDUAL_MAP',
+  option,
+  locationType
+});
+
+export const loadYourMapFailure = (option, locationType) => ({
+  type: 'LOAD_MAP_FAILURE',
+  option,
+  locationType
 });
--- src/reducers/EndpointSearchReducers.js
+++ src/reducers/EndpointSearchReducers.js
@@ -12,7 +12,59 @@ export const yourTextLocation = (state = '', action) => {
   }
 };
 
-export const friendLocation = (state = '', action) => {
+export const yourGPSLocation = (state = {lat: 0, lng: 0}, action) => {
+  switch (action.type) {
+  case 'SET_GPS_LOCATION': {
+    if (action.locationType === 'Your'){
+      return action.GPS;
+    } else {
+      return state;
+    }
+  }
+  default:
+    return state;
+  }
+};
+
+export const loadingYourMap = (state = false, action) => {
+  switch (action.type) {
+  case 'LOAD_INDIVIDUAL_MAP': {
+    if (action.locationType === 'Your') {
+      return action.option;
+    } else {
+      return state;
+    }
+  }
+  default:
+    return state;
+  }
+};
+
+export const displayYourMap = (state = false, action) => {
+  switch (action.type) {
+  case 'DISPLAY_INDIVIDUAL_MAP': {
+    if (action.locationType === 'Your') {
+      return action.option;
+    } else {
+      return state;
+    }
+  }
+  default:
+    return state;
+  }
+};
+
+export const loadYourMapFailure = (state = false, action) => {
+  switch (action.type) {
+  case 'LOAD_MAP_FAILURE': {
+    return action.locationType === 'Your' ? action.option : state;
+  }
+  default:
+    return state;
+  }
+};
+
+export const friendTextLocation = (state = '', action) => {
   switch (action.type) {
   case 'UPDATE_LOCATION': {
     if (action.locationType === 'Friend'){
@@ -26,10 +78,24 @@ export const friendLocation = (state = '', action) => {
   }
 };
 
-export const loadYourMap = (state = false, action) => {
+export const friendGPSLocation = (state = {lat: 0, lng: 0}, action) => {
+  switch (action.type) {
+  case 'SET_GPS_LOCATION': {
+    if (action.locationType === 'Friend'){
+      return action.GPS;
+    } else {
+      return state;
+    }
+  }
+  default:
+    return state;
+  }
+};
+
+export const loadingFriendMap = (state = false, action) => {
   switch (action.type) {
   case 'LOAD_INDIVIDUAL_MAP': {
-    if (action.mapType === 'Your') {
+    if (action.locationType === 'Friend') {
       return action.option;
     } else {
       return state;
@@ -40,14 +106,24 @@ export const loadYourMap = (state = false, action) => {
   }
 };
 
-export const loadFriendMap = (state = false, action) => {
+export const displayFriendMap = (state = false, action) => {
   switch (action.type) {
-  case 'LOAD_INDIVIDUAL_MAP': {
-    if (action.mapType === 'Friend') {
+  case 'DISPLAY_INDIVIDUAL_MAP': {
+    if (action.locationType === 'Friend') {
       return action.option;
     } else {
       return state;
     }
+  }
+  default:
+    return state;
+  }
+};
+
+export const loadFriendMapFailure = (state = false, action) => {
+  switch (action.type) {
+  case 'LOAD_MAP_FAILURE': {
+    return action.locationType === 'Friend' ? action.option : state;
   }
   default:
     return state;
