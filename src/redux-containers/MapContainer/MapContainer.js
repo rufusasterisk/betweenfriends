9e2882813748b5acb7b26b5c7a393ed3713e1c71
Rufus <rufusasterisk@gmail.com>
2017-10-31 14:14:59 -0600

    Add main map and container

---
+++ src/redux-containers/MapContainer/MapContainer.js
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import {
+  setGPSLocation,
+  displayMap } from '../../actions/EndpointSearchActions';
+
+import MainMap from '../../components/MainMap/MainMap';
+
+const mapStatetoProps = store => ({
+  yourGPSLocation: store.yourGPSLocation,
+  friendGPSLocation: store.friendGPSLocation,
+  mainGPSLocation: store.mainGPSLocation,
+  displayYourMap: store.displayYourMap,
+  displayFriendMap: store.displayFriendMap,
+  displayMainMap: store.displayMainMap
+});
+
+const mapDispatchtoProps = dispatch => ({
+  setMainMapGPS: (locationGPS) => {
+    dispatch(setGPSLocation(locationGPS, 'main'));
+  },
+  displayMap: (option) => {
+    dispatch(displayMap(option, 'main'));
+  }
+});
+
+
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(MainMap);
