9b5dad126f4f1d3f0c5e29165611aaa86e49c884
Rufus <rufusasterisk@gmail.com>
2017-10-30 20:02:38 -0600

    Clean code, Add missed files from last commit

--- src/actions/EndpointSearchActions.js
+++ src/actions/EndpointSearchActions.js
@@ -1,3 +1,6 @@
+import locationCleaner from '../utilities/locationCleaner';
+import { latlngAPIKey } from '../utilities/apiKey';
+
 export const updateIndividualLocation = (locationString, locationType) => ({
   type: 'UPDATE_LOCATION',
   locationString,
@@ -22,8 +25,31 @@ export const displayMap = (option, locationType) => ({
   locationType
 });
 
-export const loadYourMapFailure = (option, locationType) => ({
+export const loadMapFailure = (option, locationType) => ({
   type: 'LOAD_MAP_FAILURE',
   option,
   locationType
 });
+
+export const getMap = (locationString, locationType) => (dispatch) => {
+  dispatch(shouldLoadMap(true, locationType));
+  const cleanedQuery = locationCleaner(locationString);
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=
+    ${cleanedQuery}&key=${latlngAPIKey}`)
+    .then(response => response.json())
+    .then(resultsObject => {
+      const latlngObject = {
+        lat: resultsObject.results[0].geometry.location.lat,
+        lng: resultsObject.results[0].geometry.location.lng
+      };
+      dispatch(updateIndividualLocation(locationString, locationType));
+      dispatch(setGPSLocation(latlngObject, locationType));
+      dispatch(shouldLoadMap(false, locationType));
+      dispatch(displayMap(true, locationType));
+    })
+    .catch(error => {
+      alert(error);
+      dispatch(loadMapFailure(true, locationType));
+      dispatch(shouldLoadMap(false, locationType));
+    });
+};
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import EndpointSearchContainer from
-  '../../redux-containers/EndpointSearchContainer/EndpointSearchContainer';
+// import EndpointSearchContainer from
+//   '../../redux-containers/EndpointSearchContainer/EndpointSearchContainer';
+import YourEndpointSearchContainer from
+  '../../redux-containers/EndpointSearchContainer/YourEndpointSearchContainer';
+import FriendEndpointSearchContainer from
+  '../../redux-containers/EndpointSearchContainer/FriendEndpointSearchContainer';
+
 import Header from '../Header/Header';
-import PlacesSearch from '../PlacesSearch/PlacesSearch'
+import PlacesSearch from '../PlacesSearch/PlacesSearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <EndpointSearchContainer
+        <YourEndpointSearchContainer
           className={'your-location'}
           locationType={'Your'} />
-        <EndpointSearchContainer
+        <FriendEndpointSearchContainer
           className={'friend-location'}
           locationType={`Friend`} />
         <PlacesSearch
